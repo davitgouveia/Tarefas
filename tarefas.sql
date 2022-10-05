a3e4a59c2c9af3d4bcc7a3167aa86dbf13bb0cc1
Davi Gouveia <75867613+davitgouveia@users.noreply.github.com>
2022-10-05 11:21:26 -0300

    Update tarefas.sql

--- tarefas.sql
+++ tarefas.sql
@@ -13,5 +13,16 @@ CREATE TABLE tb.tarefa (
 	prioriTarefa VARCHAR (100),
 	tituloTarefa VARCHAR (100),
 	descTarefa VARCHAR (100),
-	dataCriTarefa DATE,	
+	statusTarefa VARCHAR (30),
+	dataCriTarefa DATE,
+	userTarefa VARCHAR (100)
+	
+--Como atribuir mais de um usuario à tarefa? Pensei em, com a ajuda do app, ao selecionar mais de um usuário, uma nova tarefa seria criada
+--para cada usuário selecionado, (automaticamente preenchendo os mesmos valores e o nome do usuário em "userTarefa"). Porem, desse jeito
+--não haveria a questão de so poder apagar a tarefa se todos concordarem.
 );
+
+ALTER TABLE tb.tarefa
+ADD CONSTRAINT fk_UserTarefa FOREIGN KEY (userTarefa) REFERENCES tb.usuario (idUser);
+ADD CONSTRAINT prioriTarefa CHECK ((prioriTarefa = 'BAIXA') OR (prioriTarefa = 'ALTA'));
+ADD CONSTRAINT statusTarefa CHECK ((statusTarefa = 'ABERTA') OR (statusTarefa = 'EM ANDAMENTO') OR (statusTarefa = 'CONCLUIDA') OR (statusTarefa = 'EXCLUIDA'));
