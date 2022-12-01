a2f42994ddaf5ca29f06a2d36f3a5919e4039587
Davi Gouveia <75867613+davitgouveia@users.noreply.github.com>
2022-12-01 09:56:29 -0300

    Update tarefas.sql
    Organização e adição de funçoes de tarefas (criarTarefa, add_user_tarefa)

--- tarefas.sql
+++ tarefas.sql
@@ -14,8 +14,8 @@ CREATE TABLE tb.tarefa (
 	descTarefa VARCHAR (100),
 	tipoTarefa VARCHAR (100),
 	statusTarefa VARCHAR (30),
-	dataCriTarefa TIMESTAMP
-	idCasaTarefa INT
+	dataTarefa TIMESTAMP,
+	idCasaTarefa INT --Ver com o mateus se deve ser NULL
 );
 
 CREATE TABLE tb.casa (
@@ -29,7 +29,7 @@ CREATE TABLE tb.userCasa (
 	idCasaCon INT,
 	userCasa INT,
 	adminCasa BOOLEAN DEFAULT('FALSE'),
-	statusUsuarioConCasa BOOLEAN DEFAULT('TRUE') --(1) ATIVO (0) DESATIVO
+	statusUsuarioConCasa BOOLEAN DEFAULT('TRUE')
 );
 
 CREATE TABLE tb.conviteCasa (
@@ -42,53 +42,26 @@ CREATE TABLE tb.conviteCasa (
 CREATE TABLE tb.userTarefa (
 	idTarefaCon INT,
 	userTarefa INT,
-	concluirTarefa VARCHAR(3) DEFAULT('NAO'),
-	apagarTarefa VARCHAR(3) DEFAULT('NAO')
-
+	concluirTarefa BOOLEAN DEFAULT('NO'),
+	apagarTarefa BOOLEAN DEFAULT('NO')
 );
 
---Workflow, caso a tarefa seja prioridade alta, o comando abaixo so rodara se todos concordarem. (revisar a questao de concluir caso nao seja ALTA)
---Função de conclusao da tarefa
-CREATE OR REPLACE FUNCTION tb.check_conclusao_prioridade(p_idTarefa INT) RETURNS void AS $$
-BEGIN
-	IF EXISTS (SELECT prioriTarefa FROM tb.tarefa WHERE 'ALTA' IN (prioriTarefa) AND idTarefa = p_idTarefa) THEN
-		PERFORM tb.check_conclusao(p_idTarefa);
-	ELSE
-		UPDATE tb.tarefa SET statusTarefa = 'CONCLUIDO' WHERE idTarefa = p_idTarefa;
-	END IF;
-END; 
-$$ LANGUAGE plpgsql;
 
---Função de mudar para concluida se todos concordarem:
-CREATE FUNCTION tb.check_conclusao(p_idTarefa INT) RETURNS void AS $$
-DECLARE resultado VARCHAR(1) := (
-SELECT CASE
-         WHEN NOT EXISTS(SELECT *
-                         FROM   tb.userTarefa
-                         WHERE  concluirTarefa <> 'SIM' AND idTarefaCon = p_idTarefa) THEN 'Y'
-         ELSE 'N'
-       END);
-BEGIN	   
-IF resultado = 'Y' THEN 
-	UPDATE tb.tarefa SET statusTarefa = 'CONCLUIDO' WHERE idTarefa = p_idTarefa;
-END IF;
-END; 
-$$ LANGUAGE plpgsql;
-
+----------------------------------FUNÇÕES DE CASA------------------------------------
 
 --Funcão de criar casa
-CREATE OR REPLACE FUNCTION criar_casa(p_nomeCasa VARCHAR(100), p_descCasa VARCHAR(100), p_userID INT) RETURNS void AS $$
+CREATE OR REPLACE FUNCTION tb.criar_casa(p_nomeCasa VARCHAR(100), p_descCasa VARCHAR(100), p_userID INT) RETURNS void AS $$
 DECLARE p_idCasa INT;
 BEGIN
 	INSERT INTO tb.casa (nomeCasa, descCasa)
 		VALUES (p_nomeCasa, p_descCasa)
 		RETURNING idCasa INTO p_idCasa;
-	PERFORM add_user_casa(p_idCasa, p_userID, 'TRUE');
+	PERFORM tb.add_user_casa(p_idCasa, p_userID, 'TRUE');
 END; 
 $$ LANGUAGE plpgsql;
 
 --Função de adicionar usuario a casa
-CREATE OR REPLACE FUNCTION add_user_casa(p_idCasaCon INT, p_userCasa INT, p_adminCasa BOOLEAN ) RETURNS void AS $$
+CREATE OR REPLACE FUNCTION tb.add_user_casa(p_idCasaCon INT, p_userCasa INT, p_adminCasa BOOLEAN ) RETURNS void AS $$
 BEGIN
 	INSERT INTO tb.userCasa (idCasaCon, userCasa, adminCasa)
 		VALUES (p_idCasaCon, p_userCasa, p_adminCasa);
@@ -103,12 +76,63 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+----------------------------------FIM FUNÇÕES DE CASA--------------------------------
+--=================================================================================--
+----------------------------------FUNÇÕES DE TAREFA----------------------------------
 
+--Funcao de criar tarefa
+CREATE OR REPLACE FUNCTION tb.criarTarefa(p_prioriTarefa VARCHAR(100), p_tituloTarefa VARCHAR(100), p_descTarefa VARCHAR(100), p_tipoTarefa VARCHAR(100), p_statusTarefa VARCHAR(30), p_dataTarefa TIMESTAMP, p_idCasaTarefa INT, p_idUserTarefa INT) RETURNS void AS $$
+DECLARE p_idTarefa INT;
+		p_concluirTarefa BOOLEAN = 'NO';
+		p_apagarTarefa BOOLEAN = 'NO';
+BEGIN
+	INSERT INTO tb.tarefa(prioritarefa, titulotarefa, desctarefa, tipotarefa, statustarefa, datatarefa, idcasatarefa)
+	VALUES (p_prioriTarefa, p_tituloTarefa, p_descTarefa, p_tipoTarefa, p_statusTarefa, p_dataTarefa, p_idCasaTarefa)
+	RETURNING idTarefa INTO p_idTarefa;
+	PERFORM tb.add_user_tarefa(p_idTarefa, p_idUserTarefa, p_concluirTarefa, p_apagarTarefa);
+END;
+$$ LANGUAGE plpgsql;
+--Funcão adicionar usuário a tarefa
+CREATE OR REPLACE FUNCTION tb.add_user_tarefa(p_idTarefaCon INT, p_idUserTarefa INT, p_concluirTarefa BOOLEAN, p_apagarTarefa BOOLEAN) RETURNS void AS $$
+BEGIN
+	INSERT INTO tb.userTarefa(idtarefacon, usertarefa, concluirtarefa, apagartarefa)
+	VALUES (p_idTarefaCon, p_idUserTarefa, p_concluirTarefa, p_apagarTarefa);
+END;
+$$ LANGUAGE plpgsql;
+
+--											!!!!LIMITAR CONEXÕES DUPLICADAS!!!!
 --
 
---------- FAZER FUNCAO DE ADICIONAR USUARIO A TAREFA
+--Função de conclusao da tarefa
+--Workflow, caso a tarefa seja prioridade alta, o comando abaixo so rodara se todos concordarem.
+CREATE OR REPLACE FUNCTION tb.check_conclusao_prioridade(p_idTarefa INT) RETURNS void AS $$
+BEGIN
+	IF EXISTS (SELECT prioriTarefa FROM tb.tarefa WHERE 'ALTA' IN (prioriTarefa) AND idTarefa = p_idTarefa) THEN
+		PERFORM tb.check_conclusao(p_idTarefa);
+	ELSE
+		UPDATE tb.userTarefa SET concluirTarefa = 'YES' WHERE idTarefaCon = p_idTarefa;
+		UPDATE tb.tarefa SET statusTarefa = 'CONCLUIDO' WHERE idTarefa = p_idTarefa;
+	END IF;
+END; 
+$$ LANGUAGE plpgsql;
 
---
+--Função de mudar para concluida se todos concordarem
+CREATE FUNCTION tb.check_conclusao(p_idTarefa INT) RETURNS void AS $$
+DECLARE resultado VARCHAR(1) = (
+SELECT CASE
+         WHEN NOT EXISTS(SELECT 
+                         FROM   tb.userTarefa
+                         WHERE  concluirTarefa = 'YES' AND idTarefaCon = p_idTarefa) THEN 'Y'
+         ELSE 'N'
+       END);
+BEGIN	   
+IF resultado = 'Y' THEN 
+	UPDATE tb.tarefa SET statusTarefa = 'CONCLUIDO' WHERE idTarefa = p_idTarefa;
+END IF;
+END; 
+$$ LANGUAGE plpgsql;
+
+----------------------------FIM DE FUNÇÕES DE TAREFA----------------------------------
 
 --Adicionando Constrains
 ALTER TABLE tb.tarefa
@@ -120,8 +144,6 @@ ALTER TABLE tb.userTarefa
 ADD CONSTRAINT fk_idTarefa FOREIGN KEY (idTarefaCon) REFERENCES tb.tarefa (idTarefa);
 ALTER TABLE tb.userTarefa 
 ADD CONSTRAINT fk_UserTarefa FOREIGN KEY (userTarefa) REFERENCES tb.usuario (idUser);
-ALTER TABLE tb.userTarefa 
-ADD CONSTRAINT apagarTarefa CHECK ((apagarTarefa = 'SIM' OR (apagarTarefa = 'NAO')));
 
 --Chave estrangeira UsuarioCasa e idUsuario
 ALTER TABLE tb.userCasa
@@ -149,22 +171,23 @@ ADD CONSTRAINT fk_CasaConvite FOREIGN KEY (idCasaConvite) REFERENCES tb.casa(idC
 
 
 --Comando de busca por usuario
-SELECT * FROM tb.tarefa AS tr INNER JOIN tb.userTarefa AS utr ON tr.idTarefa = utr.idTarefa WHERE userTarefa = 'idUsuarioCadastrado';
+SELECT  FROM tb.tarefa AS tr INNER JOIN tb.userTarefa AS utr ON tr.idTarefa = utr.idTarefa WHERE userTarefa = 'idUsuarioCadastrado';
 
 --MENU
 --Tarefas atrasadas
-SELECT * FROM tb.tarefa AS tr INNER JOIN tb.userTarefa AS utr ON tr.idTarefa = utr.idTarefa WHERE userTarefa = 'idUsuarioCadastrado' AND tr.dataCriTarefa < CURRENT_TIMESTAMP;
+SELECT * FROM tb.tarefa AS tr INNER JOIN tb.userTarefa AS utr ON tr.idTarefa = utr.idTarefaCon 
+	WHERE userTarefa = 'idUsuarioCadastrado' AND tr.dataTarefa < CURRENT_TIMESTAMP ORDER BY tr.dataTarefa ASC;
 
 --Tarefas de hoje
-SELECT * FROM tb.tarefa AS tr INNER JOIN tb.userTarefa AS utr ON tr.idTarefa = utr.idTarefa WHERE userTarefa = 'idUsuarioCadastrado' AND tr.dataCriTarefa::DATE = CURRENT_DATE
+SELECT  FROM tb.tarefa AS tr INNER JOIN tb.userTarefa AS utr ON tr.idTarefa = utr.idTarefaCon WHERE userTarefa = 'idUsuarioCadastrado' AND tr.dataTarefa::DATE = CURRENT_TIMESTAMP;
 
 --Comando para concluir tarefa
 UPDATE tb.tarefa SET statusTarefa = 'CONCLUIDO' WHERE idTarefa = '' AND 
 
 --Comando do usuario concluir tarefa compartilhada
-UPDATE tb.usertarefa SET apagarTarefa = 'SIM' WHERE idTarefa = 'idTarefa' AND userTarefa = 'idUsuarioCadastrado';
+UPDATE tb.usertarefa SET apagarTarefa = 'YES' WHERE idTarefa = 'idTarefa' AND userTarefa = 'idUsuarioCadastrado';
 
---Workflow: usuário cria nova tarefa, escolhe a qual casa pertence a tarefa.
+--Workflow usuário cria nova tarefa, escolhe a qual casa pertence a tarefa.
 --Casas do usuário
 SELECT nomeCasa FROM tb.casa AS casa INNER JOIN tb.userCasa AS ucasa ON casa.idCasa = ucasa.idCasaCon WHERE ucasa.userCasa = 'idUsuarioCadastrado';
 
@@ -174,5 +197,5 @@ SELECT nomeUser FROM tb.usuario AS us INNER JOIN tb.userCasa AS ucasa ON ucasa.u
 
 
 
---NOTAS DATETIME - format: YYYY-MM-DD HH:MI:SS
+--NOTAS DATETIME - format YYYY-MM-DD HHMISS
 --funcao CURRENT DATE tem q utilizar de acordo com a plataforma.
